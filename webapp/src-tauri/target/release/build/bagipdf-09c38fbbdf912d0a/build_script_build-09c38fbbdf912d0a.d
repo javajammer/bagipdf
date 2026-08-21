941704ba8c6433c137990b2082b5d9df51f9941b
Java Jammer <fahrizal@infokes.co.id>
2026-08-21 15:33:15 +0700

    chore(release): bump BagiPDF version to v2.4.0 with Neon DB online license gate

---
+++ webapp/src-tauri/target/release/build/bagipdf-09c38fbbdf912d0a/build_script_build-09c38fbbdf912d0a.d
@@ -0,0 +1,5 @@
+/home/rizal/Documents/PersonalAI/pdfSplit/webapp/src-tauri/target/release/build/bagipdf-09c38fbbdf912d0a/build_script_build-09c38fbbdf912d0a.d: build.rs
+
+/home/rizal/Documents/PersonalAI/pdfSplit/webapp/src-tauri/target/release/build/bagipdf-09c38fbbdf912d0a/build_script_build-09c38fbbdf912d0a: build.rs
+
+build.rs:
