2286b8c7a60f112a5f5070d6e6e241b91f0a29e8
Java Jammer <fahrizal@infokes.co.id>
2026-08-21 16:17:18 +0700

    chore(release): bump BagiPDF version to v2.4.2 with Lock & Unlock feature and high-res PDF decrypt pipeline

---
+++ webapp/src-tauri/target/release/build/bagipdf-f66282f14acebce7/build_script_build-f66282f14acebce7.d
@@ -0,0 +1,5 @@
+/home/rizal/Documents/PersonalAI/pdfSplit/webapp/src-tauri/target/release/build/bagipdf-f66282f14acebce7/build_script_build-f66282f14acebce7.d: build.rs
+
+/home/rizal/Documents/PersonalAI/pdfSplit/webapp/src-tauri/target/release/build/bagipdf-f66282f14acebce7/build_script_build-f66282f14acebce7: build.rs
+
+build.rs:
